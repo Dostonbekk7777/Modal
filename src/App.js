57b7184c2413e5f12d3347b60e7d7a450903c681
Doston <99421262+Dostonbekk7777@users.noreply.github.com>
2022-09-18 04:59:55 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './index.scss';
+import {useState} from "react"
+
+function App() {
+  const [open, setOpen] = useState(false);
+  return (
+    <div className="App">
+      <button className="open-modal-btn" onClick={()=> setOpen(true)}>✨ OPEN WINDOW</button>
+      <div className={open ? "overlay animated show" : "overlay animated"}>
+        <div className="modal">
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setOpen(false)}>
+            <title />
+            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+          </svg>
+          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
